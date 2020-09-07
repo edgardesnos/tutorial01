cc902d606e6583ebf0aeea75e60713d6273a3336
Edgar Desnos <edgar.desnos@gmail.com>
2020-09-07 17:32:09 -0400

    Student class constructor

---
+++ TestGit/src/test/git/Student.java
@@ -0,0 +1,22 @@
+package test.git;
+
+public class Student {
+	
+	String name;
+	
+	public Student() 
+	{
+		
+	}
+	
+	public String getName() 
+	{
+		return name;
+	}
+	
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+}
